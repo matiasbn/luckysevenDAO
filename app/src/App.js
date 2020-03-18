e5599beb584dae5b35cc8013a02a2ed6bc70af60
Matías Barrios <matiasbn@MacBook-Air-de-Matias.local>
2020-03-18 13:05:46 -0300

    feat: app initiated

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'typeface-roboto';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+
+function App() {
+  return (
+    <div class="container">
+    </div>
+  );
+}
+
+export default App;
